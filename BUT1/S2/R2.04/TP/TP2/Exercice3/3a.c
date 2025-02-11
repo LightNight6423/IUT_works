33df892b35286eb184ebe81df9cfad4b27935dee
DUCOURET Alexandre <duc6423alex@gmail.com>
2025-02-11 16:49:49 +0100

    tp c

---
+++ BUT1/S2/R2.04/TP/TP2/Exercice3/3a.c
@@ -0,0 +1,26 @@
+//
+// Created by didry on 11/02/2025.
+//
+//Saisie chaine transformation en majuscule en utiilisant pointeurs
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    char *p;
+
+    printf("Entrez une chaine : ");
+    scanf("%s", str); //
+
+    p = str;
+
+    while (*p != '\0') {
+        if (*p >= 'a' && *p <= 'z') {
+            *p = *p - 32;
+        }
+        p++;
+    }
+
+    printf("Chaine en majuscule: %s\n", str);
+    return 0;
+}
---
+++ BUT1/S2/R2.04/TP/TP2/Exercice3/3b.c
@@ -0,0 +1,28 @@
+//
+// Created by didry on 11/02/2025.
+//
+//Saisie chaine puis transfo avec une fonction de minuscule à majuscule avec pointeurs
+
+#include <stdio.h>
+
+void majuscule(char *str) {
+    char *p = str;
+    while (*p != '\0') {
+        if (*p >= 'a' && *p <= 'z') {
+            *p = *p - 32;
+        }
+        p++;
+    }
+}
+
+int main() {
+    char str[100];
+
+    printf("Entrez une chaine : ");
+    gets(str);
+
+    majuscule(str);
+
+    printf("Chaine en majuscule: %s\n", str);
+    return 0;
+}
